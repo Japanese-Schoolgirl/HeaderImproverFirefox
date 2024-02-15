21b862dbab369aaf23cfb5e21f2f18068da4bfa0
ZulinZulin <zulin.godot@gmail.com>
2024-02-15 20:16:35 +0300

    1.Добавил "Вкл-Выкл" тумблер. 2.Теперь "Сохранятор" не добавляет новых заголовков. 3.Поправил стили для ФФ.

--- scripts/content.js
+++ scripts/content.js
@@ -17,19 +17,25 @@ function improveHeader(header) {
 
 let dialPosition = 10;
 let randomON = true;
+let enableM = true;
 
 // Retrieve the slider value from storage when the content script loads
-browser.storage.sync.get(['sliderValue', 'randomModeValue'], function (data) {
+browser.storage.sync.get(['sliderValue', 'randomModeValue', 'enableBtnValue'], function (data) {
   if (data.sliderValue !== undefined) {
     dialPosition = Number(data.sliderValue);
   }
   if (data.randomModeValue !== undefined) {
     randomON = data.randomModeValue;
   }
-  if (dialPosition !== 10 && dialPosition !== undefined) {
+  if (data.enableBtnValue !== undefined) {
+    enableM = data.enableBtnValue;
+  }
+  if (dialPosition !== 10 && dialPosition !== undefined && enableM == true) {
     headerToChange.forEach(element => {
       improveHeader(element);
     });
+  } else if (enableM == false) {
+    console.log('Enable is OFF');
   }
 });
 
@@ -42,12 +48,12 @@ browser.runtime.onMessage.addListener(function (message, sender, sendResponse) {
     randomON = message.randomModeValue;
     console.log('randomON = ' + randomON);
     dialPosition = Number(message.sliderValue);
-    if (dialPosition !== 10) {
-      headerToChange.forEach(element => {
-        improveHeader(element);
-      });
-    } else {
-      console.log('Cant improve:(');
-    }
+    // if (dialPosition !== 10 && enableM == true) {
+    //   headerToChange.forEach(element => {
+    //     improveHeader(element);
+    //   });
+    // } else {
+    //   console.log('Cant improve:(');
+    // }
   }
 });
--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,34 @@
 const button = document.getElementById('saveButton');
 const slider = document.getElementById('slider');
+const enableBtn = document.getElementById('enableButton');
 
 console.log('button is ' + button);
 console.log('slider is ' + slider);
+console.log('enableBtn is ' + enableBtn);
 console.log('main.js WORKS');
 
-button.addEventListener('click', function (event) {
-    browser.runtime.sendMessage({
-        clickValue: 'nword'
-    });
-});
+// button.addEventListener('click', function (event) {
+//     browser.runtime.sendMessage({
+//         clickValue: 'nword'
+//     });
+// });
 
 // Retrieve the slider value from storage when the page loads
-browser.storage.sync.get(['sliderValue', 'randomModeValue'], function (data) {
+browser.storage.sync.get(['sliderValue', 'randomModeValue', 'enableBtnValue'], function (data) {
     console.log('tried to GET');
     if (data.sliderValue !== undefined) {
         slider.value = data.sliderValue;
     }
+    
     if (data.randomModeValue !== undefined) {
         // Update randomModeStatus here
         const randomModeStatus = data.randomModeValue;
         // Set checkbox state based on randomModeStatus
         document.getElementById('randomMode').checked = randomModeStatus;
+    }
+    if (data.enableBtnValue !== undefined){
+        const enableBtnStatus = data.enableBtnValue;  
+        enableBtn.checked = data.enableBtnValue;
     }
 });
 
@@ -30,15 +37,18 @@ button.addEventListener('click', function (event) {
     const sliderValue = slider.value;
     // Get the current value of the checkbox
     const randomModeStatus = document.getElementById('randomMode').checked;
+    enableBtnStatus = enableBtn.checked;
 
     // Send a message with slider value and click value
     browser.runtime.sendMessage({
         sliderValue: sliderValue,
         clickValue: 'iClicked',
-        randomModeValue: randomModeStatus
+        randomModeValue: randomModeStatus,
+        enableBtnValue: enableBtnStatus
     });
 
     // Store the slider value and checkbox value in Firefox storage
     browser.storage.sync.set({ 'sliderValue': sliderValue });
     browser.storage.sync.set({ 'randomModeValue': randomModeStatus });
+    browser.storage.sync.set({ 'enableBtnValue' : enableBtnStatus });
 });
